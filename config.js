6c07ce10d4a836c46c43d33bcbbb4f6762c5a7d4
Aries Aprilliyan <59497658+ariesaprill96@users.noreply.github.com>
2026-03-11 14:57:55 +0700

    Update config.js

--- config.js
+++ config.js
@@ -12,10 +12,5 @@
 // ============================================================
 
 window.SUPABASE_URL  = 'https://rhdegjdncazdixurpsnl.supabase.co';
-// Contoh: 'https://rhdegjdncazdixurpsnl.supabase.co'
-
 window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZGVnamRuY2F6ZGl4dXJwc25sIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzMyMDg3NzAsImV4cCI6MjA4ODc4NDc3MH0.BvN5E1UYMbCqEYuyXn7hLslWJHhXteFWJ6AIx7XoIqs';
-// Contoh: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9...'
 
-
-https://rhdegjdncazdixurpsnl.supabase.co
