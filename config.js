a820da9a4f08d048e41337160ccd4bf82d1c5f1a
Aries Aprilliyan <59497658+ariesaprill96@users.noreply.github.com>
2026-03-12 08:54:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,3 @@
-// ============================================================
-//  ARSIPARIS RI — Konfigurasi Supabase
-//  Edit file ini dengan kredensial Supabase kamu
-// ============================================================
-//
-//  Cara mendapatkan nilai ini:
-//  1. Buka https://supabase.com/dashboard
-//  2. Pilih project kamu
-//  3. Settings → API
-//  4. Copy "Project URL" dan "anon / public" key
-//
-// ============================================================
-
 window.SUPABASE_URL  = 'https://rhdegjdncazdixurpsnl.supabase.co';
-window.SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJoZGVnamRuY2F6ZGl4dXJwc25sIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MzIwODc3MCwiZXhwIjoyMDg4Nzg0NzcwfQ.7DGitymjQDJuQFIQTiIUZ_YhfkSNAMrEj8cWgJWC83E';
+window.SUPABASE_ANON_KEY = 'sb_secret_A-DHYqbdYPT3NoGapttPqQ_2VdRA0H_';
 
